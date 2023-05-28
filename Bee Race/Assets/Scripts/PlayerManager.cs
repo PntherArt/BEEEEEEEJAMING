0fafc3869c67220908b1c8b46d06f68c6d7bff06
PntherArt <PntherArt@gmail.com>
2023-05-28 14:56:32 +1000

    player selection

--- Bee Race/Assets/Scripts/PlayerManager.cs
+++ Bee Race/Assets/Scripts/PlayerManager.cs
@@ -3,27 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
+    private List<PlayerConfiguration> PlayerConfigs;
+
+    [SerializeField] private int MaxPlayers = 2;
+
     public GameObject startCam;
-   [SerializeField] private List<PlayerInput> players = new List<PlayerInput> ();
+    [SerializeField] private List<PlayerInput> players = new List<PlayerInput> ();
     [SerializeField] private List<Transform> startingPoints;
 
     public AudioSource snd;
     public AudioClip[] themes;
 
+    public static PlayerManager Instance { get; private set; }
+
     //public TextMeshProUGUI playerTag;
 
-    private PlayerInputManager playerInputManager;
+    private PlayerInputManager PlayerInputManager;
 
     private void Awake()
     {
-        playerInputManager = FindObjectOfType<PlayerInputManager> ();
-        RandomTheme();
+        //playerInputManager = FindObjectOfType<PlayerInputManager> ();
+
+
+        if(Instance != null)
+        {
+            print("SINGLETON - ERRRRROR");
+
+        } else
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+            PlayerConfigs = new List<PlayerConfiguration> ();
+        }
     }
 
-    private void OnEnable()
+    /*private void OnEnable()
     {
         playerInputManager.onPlayerJoined += AddPlayer;
         startCam.SetActive (false);
@@ -39,7 +58,7 @@ public class PlayerManager : MonoBehaviour
         players.Add(player);
         player.transform.position = startingPoints[players.Count - 1].position;
         //playerTag.text = player.name;
-    }
+    }*/
 
     public void RandomTheme()
     {
@@ -47,4 +66,45 @@ public class PlayerManager : MonoBehaviour
         snd.Play();
     }
 
+    public void SetPlayerColour(int index, Material colour)
+    {
+        colour = PlayerConfigs[index].PlayerMat;
+    }
+
+    public void ReadyPlayer(int index)
+    {
+        PlayerConfigs[index].IsReady = true;
+
+        if(PlayerConfigs.Count == MaxPlayers && PlayerConfigs.All(p => p.IsReady == true))
+        {
+            SceneManager.LoadScene("MainGame");
+            RandomTheme();
+        }
+    }
+
+    public void HandlePlayerJoin(PlayerInput pi)
+    {
+        print("Player Joined" + pi.playerIndex);
+        if (!PlayerConfigs.Any(p => p.PlayerIndex == pi.playerIndex))
+        {
+            pi.transform.SetParent(transform);
+            PlayerConfigs.Add(new PlayerConfiguration(pi));
+        }
+    }
+
+}
+
+public class PlayerConfiguration
+{
+    
+    public PlayerInput Input { get; set; }
+    public int PlayerIndex { get; set; }
+    public bool IsReady { get; set; }
+    public Material PlayerMat { get; set; }
+
+    public PlayerConfiguration(PlayerInput pi)
+    {
+        PlayerIndex = pi.playerIndex;
+        Input = pi;
+    }
 }
